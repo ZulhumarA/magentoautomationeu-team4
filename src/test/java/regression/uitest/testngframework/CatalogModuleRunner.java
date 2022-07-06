048c0fb1b81a95828538024c1711a05b283a554a
Shohret.Rejep <sohretrecep158@gmail.com>
2022-07-06 23:58:26 +0300

    UpdateAnExistingCustomerPage shohret

--- src/test/java/regression/uitest/testngframework/CatalogModuleRunner.java
+++ src/test/java/regression/uitest/testngframework/CatalogModuleRunner.java
@@ -1,4 +1,39 @@
 package regression.uitest.testngframework;
 
-public class CatalogModuleRunner {
+import com.seleniummaster.configutility.AdminLoginPage;
+import com.seleniummaster.configutility.ApplicationConfig;
+import com.seleniummaster.configutility.TestBase;
+import com.seleniummaster.configutility.TestUtility;
+import com.seleniummaster.ui.backend.catalogmodule.AddRootCategories;
+import org.testng.Assert;
+import org.testng.ITestContext;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class CatalogModuleRunner extends TestBase {
+    TestUtility testUtility;
+    AdminLoginPage adminLoginPage;
+
+    @BeforeClass
+    public void setUp(ITestContext context) {
+        testUtility = new TestUtility(driver);
+
+        browserSetUp(ApplicationConfig.readFromConfigProperties("config.properties", "backEndURL"));
+        context.setAttribute("driver", driver);
+        adminLoginPage = new AdminLoginPage(driver);
+        adminLoginPage.adminLogin("catalogManager");
+    }
+
+    @Test
+    public void AddRootCategories() {
+        AddRootCategories addRootCategories = new AddRootCategories(driver);
+        addRootCategories.NewRootCategoryInformation("Car");
+        Assert.assertTrue(addRootCategories.VerifyAddRootCategoriesSuccessfully());
+    }
+
+    @AfterClass
+    public void tearDown() {
+        closeBrowser();
+    }
 }
