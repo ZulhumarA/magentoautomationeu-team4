0ebf2809ef878d903cd95564106db3b9b38c6f56
Acer <arzugulmahmud2014@gmail.com>
2022-07-13 14:59:49 +0300

    customer manager can FilterCustomersByEmailPage-Arzugul

--- src/main/java/com/seleniummaster/ui/backend/customersmodule/FilterCustomersByEmailPage.java
+++ src/main/java/com/seleniummaster/ui/backend/customersmodule/FilterCustomersByEmailPage.java
@@ -3,6 +3,7 @@ package com.seleniummaster.ui.backend.customersmodule;
 import com.seleniummaster.configutility.ApplicationConfig;
 import com.seleniummaster.configutility.TestBase;
 import com.seleniummaster.configutility.TestUtility;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -33,13 +34,25 @@ public class FilterCustomersByEmailPage extends TestBase {
 
     public void filterEmail(){
         testUtility.waitForElementPresent(emailField);
-        emailField.sendKeys(ApplicationConfig.readFromConfigProperties("config.properties","email"));
+        emailField.sendKeys(ApplicationConfig.readFromConfigProperties("config.properties","emailAddress2"));
         testUtility.waitForElementPresent(searchButton);
         searchButton.click();
     }
-    public void verifyCustomersByEmail(){
+    public boolean verifyCustomersByEmail(){
         testUtility.waitForElementPresent(CustomerEmail);
 
+        boolean textFound = false;
+        
+        try{
+             driver.findElement(By.xpath("//*[contains(text(),'someText')]"));
+            textFound = true;
         }
+        catch (Exception e) {
+        textFound = false;
+    }
+
+
+        return textFound;
+    }
     }
 
