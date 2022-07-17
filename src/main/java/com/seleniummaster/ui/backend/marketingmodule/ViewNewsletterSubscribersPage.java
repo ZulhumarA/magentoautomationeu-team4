9b469a98fc9ad240e22b3f14878eb30fee11a63c
Gvlmihre <Gulmihre.SDET@gmail.com>
2022-07-17 19:52:34 +0300

    view newsletter subscribers page; add view newsletter subs

---
+++ src/main/java/com/seleniummaster/ui/backend/marketingmodule/ViewNewsletterSubscribersPage.java
@@ -0,0 +1,54 @@
+package com.seleniummaster.ui.backend.marketingmodule;
+
+import com.seleniummaster.configutility.TestUtility;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.Objects;
+
+public class ViewNewsletterSubscribersPage {
+
+    WebDriver driver;
+
+    TestUtility testUtility;
+
+    String statusOfCustomer;
+
+    public ViewNewsletterSubscribersPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+        testUtility = new TestUtility(driver);
+    }
+
+    @FindBy(xpath = "//span[text()='Newsletter']")
+    WebElement newsletterButton;
+
+    @FindBy(xpath = "//span[text()='Newsletter Subscribers']")
+    WebElement newsletterSubscribersButton;
+
+    @FindBy(css = "div.content-header>table>tbody>tr>td")
+    WebElement newsletterSubscribersTitle;
+
+    @FindBy(xpath = "//*[@class='hor-scroll']/table/tbody/tr[1]/td[8]")
+    WebElement customerStatus;
+
+
+    public void viewNewsletterSubscribers(){
+        testUtility.waitForElementPresent(newsletterButton);
+        newsletterButton.click();
+        testUtility.waitForElementPresent(newsletterSubscribersButton);
+        newsletterSubscribersButton.click();
+    }
+
+    public boolean verifyNewsletterSubscribersViewed(){
+        statusOfCustomer= customerStatus.getText();
+        System.out.println("status of customer is: "+statusOfCustomer);
+        return Objects.equals(statusOfCustomer, "Subscribed");
+    }
+
+
+
+
+}
