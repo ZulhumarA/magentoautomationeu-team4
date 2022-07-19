b2fbb635c5ace7d4722608e003ee81605e2edecd
Acer <arzugulmahmud2014@gmail.com>
2022-07-19 14:19:49 +0300

    Catalog Manager can add product MAGE2022-366-Arzugul

--- src/test/java/regression/uitest/cucumberframework/LoginPage.java
+++ src/test/java/regression/uitest/cucumberframework/LoginPage.java
@@ -1,4 +1,14 @@
 package regression.uitest.cucumberframework;
 
+import com.seleniummaster.configutility.TestUtility;
+import org.openqa.selenium.WebDriver;
+
 public class LoginPage {
+    TestUtility testUtility;
+    WebDriver webDriver;
+
+
+
+
+
 }
