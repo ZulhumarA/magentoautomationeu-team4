39fb59dec940e9fdec5a38486c1b610284d79114
Gvlmihre <Gulmihre.SDET@gmail.com>
2022-07-20 18:08:11 +0300

    content of cucumber test runner

--- src/test/java/regression/uitest/cucumberframework/LoginSteps.java
+++ src/test/java/regression/uitest/cucumberframework/LoginSteps.java
@@ -1,4 +1,44 @@
 package regression.uitest.cucumberframework;
 
-public class LoginSteps {
+import com.seleniummaster.configutility.AdminLoginPage;
+import com.seleniummaster.configutility.ApplicationConfig;
+import com.seleniummaster.configutility.TestBase;
+import com.seleniummaster.configutility.TestUtility;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginSteps extends TestBase {
+
+    AdminLoginPage adminLoginPage;
+
+
+    @Given("admin user already on the login page")
+    public void adminUserAlreadyOnTheLoginPage() {
+        browserSetUp(ApplicationConfig.readFromConfigProperties("config.properties","backEndURL"));
+    }
+
+    @When("store manager enters valid username and password to click on login button")
+    public void storeManagerEntersValidUsernameAndPasswordToClickOnLoginButton() {
+    }
+
+    @Then("store manager should be able to login successfully")
+    public void storeManagerShouldBeAbleToLoginSuccessfully() {
+    }
+
+    @When("marketing manager enters valid username and password to click on login button")
+    public void marketingManagerEntersValidUsernameAndPasswordToClickOnLoginButton() {
+    }
+
+    @Then("marketing manager should be able to login successfully")
+    public void marketingManagerShouldBeAbleToLoginSuccessfully() {
+    }
+
+    @When("reporting manager enters valid username and password to click on login button")
+    public void reportingManagerEntersValidUsernameAndPasswordToClickOnLoginButton() {
+    }
+
+    @Then("reporting manager should be able to login successfully")
+    public void reportingManagerShouldBeAbleToLoginSuccessfully() {
+    }
 }
