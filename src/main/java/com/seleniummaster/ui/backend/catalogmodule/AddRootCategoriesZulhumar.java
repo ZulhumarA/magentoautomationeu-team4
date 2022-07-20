c01eeb5754b9787a0fd25843a1c5fc3dfc93ddf5
zulhu <zulhumaraman44@gmail.com>
2022-07-20 13:59:08 +0300

    update 3 tests for catalog module,Delete root categories-Zulhumar

--- src/main/java/com/seleniummaster/ui/backend/catalogmodule/AddRootCategoriesZulhumar.java
+++ src/main/java/com/seleniummaster/ui/backend/catalogmodule/AddRootCategoriesZulhumar.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 
 public class AddRootCategoriesZulhumar extends TestBase {
     TestUtility testUtility;
@@ -21,22 +22,22 @@ public class AddRootCategoriesZulhumar extends TestBase {
     @FindBy(xpath = "//input[@id=\"group_4name\"]")
     WebElement rootName;
 
-    @FindBy(id="group_4is_active")
+    @FindBy(id = "group_4is_active")
     WebElement isActive;
 
-    @FindBy(id="group_4description")
+    @FindBy(id = "group_4description")
     WebElement description;
 
-    @FindBy(id="group_4meta_title")
+    @FindBy(id = "group_4meta_title")
     WebElement pageTitle;
 
-    @FindBy(id="group_4meta_keywords")
+    @FindBy(id = "group_4meta_keywords")
     WebElement metaKeywords;
 
-    @FindBy(id="group_4meta_description")
+    @FindBy(id = "group_4meta_description")
     WebElement metaDescription;
 
-    @FindBy(xpath="//*[@title=\"Save Category\"]")
+    @FindBy(xpath = "//*[@title=\"Save Category\"]")
     WebElement saveButton;
 
     @FindBy(xpath = "//span[contains(text(),\"The category has been saved.\")]")
@@ -47,36 +48,37 @@ public class AddRootCategoriesZulhumar extends TestBase {
         PageFactory.initElements(driver, this);
         testUtility = new TestUtility(driver);
     }
-   public void openAddNewRootCategoryPage() {
-       testUtility.waitForElementPresent(catalogButton);
-       catalogButton.click();
-       testUtility.waitForElementPresent(manageCategories);
-       manageCategories.click();
-    }
-   public void fillCategoryInformationAndSave() throws InterruptedException {
+
+    public void openAddNewRootCategoryPage() throws InterruptedException {
+        testUtility.waitForElementPresent(catalogButton);
+        catalogButton.click();
         Thread.sleep(3000);
-rootName.sendKeys(testUtility.fakeCategoryName());
-       Thread.sleep(3000);
-pageTitle.sendKeys(testUtility.fakePageTitle());
-       Thread.sleep(3000);
-       metaKeywords.sendKeys(testUtility.metaKeywords());
-       Thread.sleep(3000);
-metaDescription.sendKeys(testUtility.metaDescriptions());
-testUtility.waitForElementPresent(saveButton);
-saveButton.click();
+        // testUtility.waitForElementPresent(manageCategories);
+        manageCategories.click();
     }
 
-public boolean verifyAddRootCategories() throws InterruptedException {
+    public void fillCategoryInformationAndSave() throws InterruptedException {
         Thread.sleep(3000);
-       testUtility.waitForElementPresent(successfulSavesMessage);
-    return successfulSavesMessage.isDisplayed();
-}
+        rootName.sendKeys("Zulhumar" + testUtility.fakeCategoryName() + "Team4");
+        Thread.sleep(3000);
+        testUtility.waitForElementPresent(isActive);
+        Select select = new Select(isActive);
+        select.selectByValue("1");
+        pageTitle.sendKeys("Zulhumar" + testUtility.fakePageTitle());
+        Thread.sleep(3000);
 
+        metaKeywords.sendKeys(testUtility.metaKeywords());
+        Thread.sleep(3000);
+        metaDescription.sendKeys(testUtility.metaDescriptions());
+        testUtility.waitForElementPresent(saveButton);
+        saveButton.click();
+    }
 
-
-
-
-
+    public boolean verifyAddRootCategories() throws InterruptedException {
+        Thread.sleep(3000);
+        testUtility.waitForElementPresent(successfulSavesMessage);
+        return successfulSavesMessage.isDisplayed();
+    }
 
 
 }
