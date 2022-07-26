3db6a0ba0870235abef8e0c681134c9cea849824
Gvlmihre <Gulmihre.SDET@gmail.com>
2022-07-26 09:10:10 +0300

    database test preperation and the test runner classes editing

---
+++ src/main/java/com/seleniummaster/database/ConnectionType.java
@@ -0,0 +1,5 @@
+package com.seleniummaster.database;
+
+public enum ConnectionType {
+    MSSQL, MYSQL
+}
---
+++ src/main/java/com/seleniummaster/database/DatabaseMethods.java
@@ -0,0 +1,27 @@
+package com.seleniummaster.database;
+
+import com.seleniummaster.configutility.ApplicationConfig;
+
+import java.sql.Connection;
+
+public class DatabaseMethods {
+
+    Connection connection=null;
+
+    final static String configFile="config.properties";
+
+    public void databaseConnectionSetUp(){
+        String dbUrl= ApplicationConfig.readFromConfigProperties(configFile,"dbUrl");
+        String dbPort= ApplicationConfig.readFromConfigProperties(configFile,"dbPort");
+        String defaultDatabase= ApplicationConfig.readFromConfigProperties(configFile,"defaultDatabase");
+        String dbUserName= ApplicationConfig.readFromConfigProperties(configFile,"dbUserName");
+        String dbPassword= ApplicationConfig.readFromConfigProperties(configFile,"dbPassword");
+        connection=ConnectionManager.connectToDatabaseServer(dbUrl,dbPort,defaultDatabase,dbUserName,dbPassword,ConnectionType.MYSQL);
+    }
+
+    public void closeDatabaseConnection(){
+        ConnectionManager.closeDatabaseConnection(connection);
+    }
+
+
+}
