87752620a8efced9b5b6e2c2b0a79da29d640e0c
Gvlmihre <Gulmihre.SDET@gmail.com>
2022-07-26 06:30:22 +0300

    customer runner fix to run

--- src/test/java/regression/uitest/cucumberframework/reportingmodulesteps/ReportingModuleSteps.java
+++ src/test/java/regression/uitest/cucumberframework/reportingmodulesteps/ReportingModuleSteps.java
@@ -11,7 +11,7 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.testng.Assert;
 
-public class ReportingProductsSteps extends TestBase {
+public class ReportingModuleSteps extends TestBase {
 
     AdminLoginPage adminLoginPage;
     ProductsOrderedReportPage productsOrderedReportPage;
